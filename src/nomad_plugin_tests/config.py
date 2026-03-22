48acd165d2aa8456b40b33b7a3cb95bfbb40c74e
lukaspie <lukaspie@github.com>
2026-03-22 18:18:33 +0100

    use tests/nomad folder for pynxtools plugins

--- src/nomad_plugin_tests/config.py
+++ src/nomad_plugin_tests/config.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 TESTS_TO_RUN = {
     "pynxtools": "tests/nomad",
-    "pynxtools_apm": "tests/test_nomad_examples.py",
-    "pynxtools_ellips": "tests/test_nomad_examples.py",
-    "pynxtools_em": "tests/test_nomad_examples.py",
-    "pynxtools_mpes": "tests/test_nomad_examples.py",
-    "pynxtools_stm": "tests/test_nomad_examples.py",
-    "pynxtools_spm": "tests/test_nomad_examples.py",
-    "pynxtools_xps": "tests/test_nomad_examples.py",
+    "pynxtools_apm": "tests/nomad",
+    "pynxtools_ellips": "tests/nomad",
+    "pynxtools_em": "tests/nomad",
+    "pynxtools_mpes": "tests/nomad",
+    "pynxtools_stm": "tests/nomad",
+    "pynxtools_spm": "tests/nomad",
+    "pynxtools_xps": "tests/nomad",
     "electronicparsers": "tests",
 }
 
